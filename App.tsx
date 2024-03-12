4fd2c2d11874d30aac8bdc158e9c676682df2635
andreluizborgess <andreluizborges1995@gmail.com>
2024-03-12 15:42:44 -0300

    12/03

--- App.tsx
+++ App.tsx
@@ -1,94 +1,130 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
-function App(): React.JSX.Element{
-return(
-<View style = {styles.container}>
-<View style={styles.header}>
-<Text style={styles.city}>Presidente Epitácio</Text>
-</View>
+function App(): React.JSX.Element {
+    const [city, setCity] = useState<string>("São Paulo");
+    const [humidity, setHumidity] = useState<string>("15");
+    const [condition, setCondition] = useState<string>("Ensolarado");
+    const [rainProbability, setRainProbability] = useState<string>("30");
+    const [feelsLike, setFeelsLike] = useState<string>("41");
+    const [temperature, setTemperature] = useState<string>("37");
+    const [night, setNight] = useState<boolean>(false);
 
-<View style={styles.detailsContainer}>
-<Text style={styles.temperature}>55 ºC</Text>
-<Image source={require('./src/assets/images/icon3.png')}
-style={styles.wheaterIcon}/>
-</View>
+    function isNight() {
+        const hour = new Date().getHours();
+        console.log(hour);
 
-<View>
-<Text style={styles.wheaterCondition}>nublado</Text>
-<Text style={styles.text}>sensação termica: 179ºC</Text>
-<Text style={styles.text}>probabilidade de chuva: 78%</Text>
-<Text style={styles.text}>Umidade: 99%</Text>
-</View>
-<Image source={require('./src/assets/images/cidade.png')}
-style={styles.bottomImage}/>
-</View>
+        if(hour >= 18 || hour < 6){
+            setNight(true);
+        } else {
+            setNight(false);
+        }
+    }
 
-);
+    useEffect(() => {
+        const intervalId = setInterval(()=>{
+            
+            isNight();
+
+        },60000);
+
+
+
+    });
+    return (
+        <View style={[styles.container,night == false ? styles.containerBgDay : styles.containerBgNight]}>
+            <View style={styles.header}>
+                <Text style={styles.city}>{city}</Text>
+            </View>
+
+            <View style={styles.detailsContainer}>
+                <Text style={styles.temperature}>{temperature}</Text>
+                <Image source={require('./src/assets/images/icon3.png')}
+                    style={styles.wheaterIcon} />
+            </View>
+
+            <View>
+                <Text style={styles.wheaterCondition}>{condition}</Text>
+                <Text style={styles.text}>sensação termica:{feelsLike}º</Text>
+                <Text style={styles.text}>probabilidade de chuva: {rainProbability}%</Text>
+                <Text style={styles.text}>Umidade: {humidity}%</Text>
+            </View>
+            <Image source={require('./src/assets/images/cidade.png')}
+                style={styles.bottomImage} />
+        </View>
+
+    );
 }
 
 const styles = StyleSheet.create({
-bottomImage:{
-width:'115%',
-position: 'absolute',
-bottom: 0,
-resizeMode: 'cover',
-height: '57%'
-},
-header:{
-flexDirection: 'row',
-justifyContent: 'space-between',
-marginBottom: 20
+    bottomImage: {
+        width: '115%',
+        position: 'absolute',
+        bottom: 0,
+        resizeMode: 'cover',
+        height: '57%'
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 20
 
 
-},
-city:{
-fontWeight: 'bold',
-color: '#fff',
-fontSize: 25
+    },
+    city: {
+        fontWeight: 'bold',
+        color: '#fff',
+        fontSize: 25
 
-},
-wheaterIcon:{
-width: 100,
-height: 100,
-alignSelf: 'flex-end',
-marginBottom: 20,
-flexDirection: 'row'
+    },
+    wheaterIcon: {
+        width: 100,
+        height: 100,
+        alignSelf: 'flex-end',
+        marginBottom: 20,
+        flexDirection: 'row'
 
-},
-detailsContainer:{
-flexDirection: 'row',
-justifyContent: 'space-between',
+    },
+    detailsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
 
 
-},
-wheaterCondition:{
-fontSize: 18,
-fontWeight: 'bold',
-alignSelf: 'flex-end',
-marginBottom: 20,
-color: '#fff'
+    },
+    wheaterCondition: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        alignSelf: 'flex-end',
+        marginBottom: 20,
+        color: '#fff'
 
-},
-temperature:{
-fontSize: 55,
-marginBottom: 20,
-color: '#fff'
+    },
+    temperature: {
+        fontSize: 55,
+        marginBottom: 20,
+        color: '#fff'
 
-},
-container:{
-flex:1,
-padding:20,
-backgroundColor:'#09d3f3'
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#09d3f3'
 
-},
-text:{
-fontSize:17,
-color:'#fff',
-marginBottom:10,
-fontStyle: 'italic'
-   
-}
+    },
+    text: {
+        fontSize: 17,
+        color: '#fff',
+        marginBottom: 10,
+        fontStyle: 'italic'
+
+    },
+
+    containerBgDay: {
+        backgroundColor: '#09d3f3'
+    },
+    containerBgNight: {
+        backgroundColor: '#333'
+    },
 
 });
- export default App;
+export default App;
